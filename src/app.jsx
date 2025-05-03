b9ce96926c0521c13c988eb358e43337493a3c62
Manav Misra <manavm@visionwebsoft.com>
2025-05-03 17:48:25 -0500

    feat: :tada: initial commit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h1 className="text-3xl text-red-500">Hello, World!</h1>
+      <p>This is a simple React application.</p>
+    </main>
+  )
+}
